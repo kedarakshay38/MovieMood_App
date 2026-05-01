94a201607c127f08dd71a4ae450cb5669a1d242e
Akshay Kedar <kedarakshay38@gmail.com>
2026-05-01 14:45:03 +0530

    tailwind css implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   content: [
+      "./index.html",                                                                                                                                                                                                                         
+      "./src/**/*.{js,jsx}",                                                                                                                                                                                                                  
+    ],                                                                                                                                                                                                                                        
+    darkMode: "class",                                                                                                                                                                                                                        
+    theme: {                                                                                                                                                                                                                                  
+      extend: {},                                                                                                                                                                                                                             
+    },                                                                                                                                                                                                                                        
+    plugins: [],  
 }
 
