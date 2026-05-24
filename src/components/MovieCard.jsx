ea676956816ae7a70b9fa4b07395947d5b710dd3
Akshay Kedar <kedarakshay38@gmail.com>
2026-05-24 15:19:45 +0530

    uodatubg movie card and Movie Detail

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 function MovieCard({ movie }) {
   return (
+      <Link to={`/movie/${movie.imdbID}`}>
     <div className="bg-gray-800 rounded p-2">
       <img src={movie.Poster} alt={movie.Title} className="w-full rounded" />
       <h3 className="text-white mt-2 font-semibold">{movie.Title}</h3>
       <p className="text-gray-400 text-sm">{movie.Year}</p>
     </div>
+    </Link>
   );
 }
 
